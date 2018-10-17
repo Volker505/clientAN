7bd69212fee4422ddd635ffd221a1faf4d5e1ff6
volker <sk8max@yandex.ru>
2018-10-18 01:12:54 +0300

    chat

--- src/app/na-chat/na-chat.module.ts
+++ src/app/na-chat/na-chat.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NaChatComponent } from './na-chat.component';
 import {NaChatRoutingModule} from './na-chat-routing.module';
 import {NaCommonModule} from '../core/modules/common-module/na-common.module';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
 
 @NgModule({
   imports: [
@@ -10,6 +11,6 @@ import {NaCommonModule} from '../core/modules/common-module/na-common.module';
     NaCommonModule,
     NaChatRoutingModule
   ],
-  declarations: [NaChatComponent]
+  declarations: [NaChatComponent, ChatMessageComponent]
 })
 export class NaChatModule { }
